db4447411c3ced9099d1309375975f4fe6869cd8
Colin Arrowsmith <carrowsm@h4huhnlogin2.uhnh4h.cluster>
2019-10-23 10:34:42 -0400

    First commit

---
+++ classify.py
@@ -0,0 +1,7 @@
+'''
+This script classifies images in the RadCure dataset which contain 'strong'
+metal artifact streaks.
+
+The program processes each patient slice-by-slice, and picks out slices with
+where there is very strong evidence of artifacts.
+'''
